761b5dbe95095aa06aeebd29f8ec297f9c15a8e3
Yevhenii Matviienko <yevheniimatviienko@knu.ua>
2026-07-23 12:23:54 +0300

    Bouncing Balls (6 kyu)

---
+++ Bouncing Balls (6 kyu)/Solution (C).c
@@ -0,0 +1,11 @@
+int bouncingBall(double h, double bounce, double window) {
+    if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+        return -1;
+    }
+    int ball_seen = 1;
+    while (h * bounce > window) {
+        ball_seen += 2;
+        h *= bounce;
+    }
+    return ball_seen;
+}
