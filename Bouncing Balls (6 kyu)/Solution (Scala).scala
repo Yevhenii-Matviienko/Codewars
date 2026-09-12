ee1e35237a4f5ff22e55c187bed4c1ee9cc9523c
Yevhenii Matviienko <yevheniimatviienko@knu.ua>
2026-09-12 13:18:39 +0300

    Bouncing Balls (6 kyu)

---
+++ Bouncing Balls (6 kyu)/Solution (Scala).scala
@@ -0,0 +1,14 @@
+object BouncingBall {
+    def bouncingBall(h: Double, bounce: Double, window: Double): Int = {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+            return -1
+        }
+        var height = h
+        var ball_seen = 1
+        while (height * bounce > window) {
+            ball_seen += 2
+            height *= bounce
+        }
+        return ball_seen
+    }
+}
